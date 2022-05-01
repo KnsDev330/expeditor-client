9f524e5285ffdeeffa33e75c13aa87fe2ff7bedb
Khandaker Sajjat <me@kns.dev>
2022-05-01 19:03:19 +0600

    added loading on password reset page

--- src/components/ResetPassword/ResetPassword.js
+++ src/components/ResetPassword/ResetPassword.js
@@ -1,17 +1,24 @@
 import { Button } from 'react-bootstrap';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ResetPassword.css';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase.init';
 import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import Loading from '../Loading/Loading';
 const toastConfig = { position: "top-right", autoClose: 2000 };
 
 const ResetPassword = () => {
     const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
 
     useEffect(() => { error && toast.error(`${error.code.slice(5).replace(/-/g, ' ')}`, toastConfig) }, [error]);
-    useEffect(() => { sending && toast.info(`Sending your password reset email`, toastConfig) }, [sending]);
+
+    // show loading screen when sending reset password email 
+    const [showLoading, setShowLoading] = useState(false);
+    useEffect(() => {
+        setShowLoading(sending);
+        if (sending) toast.info(`Sending password reset email`, toastConfig);
+    }, [sending]);
 
     const ResetPassword = e => {
         e.preventDefault();
@@ -24,14 +31,23 @@ const ResetPassword = () => {
             <div className='register d-flex flex-column align-items-center pt-4 pb-5 mb-5'>
 
                 <h2>Reset Password</h2>
-                <small className='mb-4'>Reset your account password</small>
+                {
+                    showLoading ?
+                        <>
+                            <p className='mt-5'><Loading></Loading></p>
+                        </>
+                        :
+                        <>
+                            <small className='mb-4'>Reset your account password</small>
 
-                <form className='w-100' onSubmit={ResetPassword}>
-                    <label htmlFor="email" className='text-label'>Email:</label>
-                    <input type="email" autoComplete='on' className='form-control mx-auto mb-2' name="email" id="email" placeholder='Enter Email' required />
-                    <Button variant='success' type='submit' className='my-3 px-5 out'>Reset</Button><br />
-                    <small>Return to <Link to='/login'>Login</Link></small><br />
-                </form>
+                            <form className='w-100' onSubmit={ResetPassword}>
+                                <label htmlFor="email" className='text-label'>Email:</label>
+                                <input type="email" autoComplete='on' className='form-control mx-auto mb-2' name="email" id="email" placeholder='Enter Email' required />
+                                <Button variant='success' type='submit' className='my-3 px-5 out'>Reset</Button><br />
+                                <small>Return to <Link to='/login'>Login</Link></small><br />
+                            </form>
+                        </>
+                }
 
             </div>
         </div >
